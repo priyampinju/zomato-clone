33e2ebc26dd7ac415e7e3451751e3c018dc7d843
priyampinju <pinjupriyam1826@gmail.com>
2024-01-31 17:36:12 +0530

    msdf

---
+++ src/pages/Index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <>
+            <div>
+                <img className='logo' src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="ufjv" />
+                <img src="https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png?output-format=webp" alt="illustration" />
+                <img src="https://b.zmtcdn.com/data/o2_assets/30fa0a844f3ba82073e5f78c65c18b371616149662.png?output-format=webp" alt="illustration2" />
+            </div>
+        </>
+    )
+}
+
+export default Index
