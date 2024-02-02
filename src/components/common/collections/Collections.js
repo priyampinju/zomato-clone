20d6acb522913910d9ea422b0fd95a25799f8818
priyampinju <pinjupriyam1826@gmail.com>
2024-02-03 00:45:53 +0530

    delivery done

--- src/components/common/collections/Collections.js
+++ src/components/common/collections/Collections.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import Slider from 'react-slick'
 import './Collections.css'
+import NextArrow from '../carousel/NextArrow'
+import PrevArrow from '../carousel/PrevArrow'
+
+const settings = {
+    infinite: false,
+    slidesToShow: 4,
+    sidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+};
 
 const Collections = ({ list }) => {
     return (
@@ -10,9 +21,25 @@ const Collections = ({ list }) => {
                     <div className='collection-subtitle-text'>Explore curated lists of top restaurants, cafes, pubs and bars in Jorhat based on trends</div>
                     <div className="collection-location">
                         <div className="collection-location-all">All collections in Jorhat</div>
-                        <i className="fi fi-rr-caret-right"></i>
+                        <i className="fi fi-rr-caret-right absolute-center"></i>
                     </div>
                 </div>
+                <Slider {...settings}>
+                    {list.map((item) => (
+                        <div>
+                            <div className="collection-cover">
+                                <img src={item.cover} alt={item.title} />
+                                <div className="gradient-bg"></div>
+                                <div className="collection-card-title">{item.title}</div>
+                                <div className="collection-card-subtitle">
+                                    <div>{item.places}</div>
+                                    <i className="fi fi-rr-caret-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                </Slider>
             </div>
         </div>
     )
--- src/components/diningout/DiningOut.js
+++ src/components/diningout/DiningOut.js
@@ -5,60 +5,46 @@ import Collections from '../common/collections/Collections'
 const collectionList= [
   {
     id: 1,
-    title: "Live Sports Screenings",
-    cover:
-      "https://b.zmtcdn.com/data/collections/7e296d5b75ca7b0f88e451b49e41ba99_1618208591.jpg",
-    places: "12 Places",
+    title: "7 Serene Rooftop Places",
+    cover: 	'https://b.zmtcdn.com/data/collections/293255cbfe49f4ebdb244c1bfc3a0f74_1691151613.jpg',
+    places: "7 Places",
   },
   {
     id: 2,
-    title: "Newly Opened",
+    title: "7 Great Buffets",
     cover:
-      "https://b.zmtcdn.com/data/collections/b22194cb38ed18a5200b387ad8f243f0_1582015789.jpg",
-    places: "12 Places",
+      "https://b.zmtcdn.com/data/collections/6d15bb3be793058440ed5e7006149c07_1691151825.jpg",
+    places: "7 Places",
   },
   {
     id: 3,
-    title: "Veggie Friendly",
+    title: "9 Best Luxury Dining Places",
     cover:
-      "https://b.zmtcdn.com/data/collections/5d13e5bef6e5a44cfb7ea19fb915a9ba_1616407816.jpg",
-    places: "23 Places",
+      "https://b.zmtcdn.com/data/collections/a1e31eb92bb9951aaf1750d88497adb4_1691151744.jpg",
+    places: "9 Places",
   },
   {
     id: 4,
-    title: "Trending This Week",
+    title: "7 Pan Asian Restaurants",
     cover:
-      "https://b.zmtcdn.com/data/collections/83fed288b6d32c932b7d3a0034c4f947_1625650348.png",
-    places: "30 Places",
+      "	https://b.zmtcdn.com/data/collections/1b3bd5bd6ff1e9e5176b81db369f1d62_1691152212.jpeg",
+    places: "7 Places",
   },
   {
     id: 5,
-    title: "Best of Bengaluru",
+    title: "9 Great cafes",
     cover:
-      "https://b.zmtcdn.com/data/collections/53be5a745e959a8f6c51c52dd3d9edd1_1536825795.jpg",
-    places: "116 Places",
+      "https://b.zmtcdn.com/data/collections/61d06b1dc0a478a6216bcf07ff8b2d67_1691151375.jpg",
+    places: "9 Places",
   },
   {
     id: 6,
-    title: "Outdoor Seating",
+    title: "7 Bingeworthy Desserts",
     cover:
-      "https://b.zmtcdn.com/data/collections/b1d388518012c80696487c08ca08e325_1582201041.jpg",
-    places: "24 Places",
+      "https://b.zmtcdn.com/data/collections/3c8d1e8d612c909438576795095ff71b_1691151059.png",
+    places: "5 Places",
   },
-  {
-    id: 7,
-    title: "Brilliant Biryanis",
-    cover:
-      "https://b.zmtcdn.com/data/collections/81dc179878a8d9e8b2ab03e4eaeab0df_1459434256.jpg",
-    places: "16 Places",
-  },
-  {
-    id: 8,
-    title: "Best Pizza Places In Town",
-    cover:
-      "https://b.zmtcdn.com/data/collections/6398cf5475ba51e532003912a8151436_1616403332.jpg",
-    places: "22 Places",
-  },
+  
 ]
 
 const DiningOut = () => {
